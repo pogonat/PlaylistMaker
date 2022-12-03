3d70c57769d6e42ba1691dc90962243dab34b207
pogonat <aimagenta81@gmail.com>
2022-12-04 01:22:33 +0600

    added RecyclerView and Glide for images

--- app/src/main/java/com/example/playlistmaker/SearchActivity.kt
+++ app/src/main/java/com/example/playlistmaker/SearchActivity.kt
@@ -6,10 +6,16 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.text.Editable
 import android.text.TextWatcher
+import android.view.LayoutInflater
 import android.view.View
+import android.view.ViewGroup
 import android.view.inputmethod.InputMethodManager
 import android.widget.EditText
 import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
 
 class SearchActivity : AppCompatActivity() {
 
@@ -49,6 +55,80 @@ class SearchActivity : AppCompatActivity() {
             }
         }
         inputEditText.addTextChangedListener(searchTextWatcher)
+
+        val recyclerView = findViewById<RecyclerView>(R.id.recycler_view_items)
+        recyclerView.layoutManager = LinearLayoutManager(this)
+
+        class TracksViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+
+            private val trackName: TextView = itemView.findViewById(R.id.source_track_name)
+            private val artistName: TextView = itemView.findViewById(R.id.source_artist_name)
+            private val trackTime: TextView = itemView.findViewById(R.id.source_track_duration)
+            private val artworkUrl100: ImageView = itemView.findViewById((R.id.source_album_art))
+
+            fun bind(model: Track) {
+                trackName.text = model.trackName
+                artistName.text = model.artistName
+                trackTime.text = model.trackTime
+                Glide.with(artworkUrl100)
+                    .load(model.artworkUrl100)
+                    .into(artworkUrl100)
+            }
+
+        }
+
+        class TrackAdapter(
+            private val tracks: ArrayList<Track>
+        ) : RecyclerView.Adapter<TracksViewHolder>() {
+            override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): TracksViewHolder {
+                val view = LayoutInflater.from(parent.context)
+                    .inflate(R.layout.view_holder_search_result, parent, false)
+                return TracksViewHolder(view)
+            }
+
+            override fun onBindViewHolder(holder: TracksViewHolder, position: Int) {
+                holder.bind(tracks[position])
+            }
+
+            override fun getItemCount(): Int {
+                return tracks.size
+            }
+        }
+
+        val trackList: ArrayList<Track> = arrayListOf(
+            Track(
+                getString(R.string.track_1_trackName),
+                getString(R.string.track_1_artistName),
+                getString(R.string.track_1_trackTime),
+                getString(R.string.track_1_artworkUrl100)
+            ),
+            Track(
+                getString(R.string.track_2_trackName),
+                getString(R.string.track_2_artistName),
+                getString(R.string.track_2_trackTime),
+                getString(R.string.track_2_artworkUrl100)
+            ),
+            Track(
+                getString(R.string.track_3_trackName),
+                getString(R.string.track_3_artistName),
+                getString(R.string.track_3_trackTime),
+                getString(R.string.track_3_artworkUrl100)
+            ),
+            Track(
+                getString(R.string.track_4_trackName),
+                getString(R.string.track_4_artistName),
+                getString(R.string.track_4_trackTime),
+                getString(R.string.track_4_artworkUrl100)
+            ),
+            Track(
+                getString(R.string.track_5_trackName),
+                getString(R.string.track_5_artistName),
+                getString(R.string.track_5_trackTime),
+                getString(R.string.track_5_artworkUrl100)
+            )
+        )
+        val trackAdapter = TrackAdapter(trackList)
+        recyclerView.adapter = trackAdapter
     }
 
     companion object {
@@ -73,39 +153,12 @@ class SearchActivity : AppCompatActivity() {
         }
     }
 
-    class Track (
+    class Track(
         val trackName: String, // Название композиции
         val artistName: String, // Имя исполнителя
         val trackTime: String, // Продолжительность трека
         val artworkUrl100: String // Ссылка на изображение обложки
     )
-
-    val trackList: ArrayList<Track> = arrayListOf(
-        Track(
-            getString(R.string.track_1_trackName),
-            getString(R.string.track_1_artistName),
-            getString(R.string.track_1_trackTime),
-            getString(R.string.track_1_artworkUrl100)),
-        Track(getString(R.string.track_2_trackName),
-            getString(R.string.track_2_artistName),
-            getString(R.string.track_2_trackTime),
-            getString(R.string.track_2_artworkUrl100)),
-        Track(getString(R.string.track_3_trackName),
-            getString(R.string.track_3_artistName),
-            getString(R.string.track_3_trackTime),
-            getString(R.string.track_3_artworkUrl100)),
-        Track(getString(R.string.track_4_trackName),
-            getString(R.string.track_4_artistName),
-            getString(R.string.track_4_trackTime),
-            getString(R.string.track_4_artworkUrl100)),
-        Track(getString(R.string.track_5_trackName),
-            getString(R.string.track_5_artistName),
-            getString(R.string.track_5_trackTime),
-            getString(R.string.track_5_artworkUrl100)
-        )
-    )
-
-
 
 }
 
