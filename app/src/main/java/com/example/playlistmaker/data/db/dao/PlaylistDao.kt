ee0d6db0468632deb3a3a15bacb8272fe1b1a88d
pogonat <aimagenta81@gmail.com>
2023-07-29 18:57:13 +0600

    Dao annotation

--- app/src/main/java/com/example/playlistmaker/data/db/dao/PlaylistDao.kt
+++ app/src/main/java/com/example/playlistmaker/data/db/dao/PlaylistDao.kt
@@ -1,10 +1,12 @@
 package com.example.playlistmaker.data.db.dao
 
+import androidx.room.Dao
 import androidx.room.Insert
 import androidx.room.OnConflictStrategy
 import androidx.room.Query
 import com.example.playlistmaker.data.db.entity.PlaylistEntity
 
+@Dao
 interface PlaylistDao {
 
     @Insert(onConflict = OnConflictStrategy.REPLACE)
