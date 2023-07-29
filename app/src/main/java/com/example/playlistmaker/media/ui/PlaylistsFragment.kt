921e11517fb4cd0ca84548858d8cd1b9ec826549
pogonat <aimagenta81@gmail.com>
2023-07-29 19:04:42 +0600

    fixes in playlist feature navigation

--- app/src/main/java/com/example/playlistmaker/media/ui/PlaylistsFragment.kt
+++ app/src/main/java/com/example/playlistmaker/media/ui/PlaylistsFragment.kt
@@ -6,6 +6,8 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.core.view.isVisible
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import com.example.playlistmaker.R
 import com.example.playlistmaker.databinding.FragmentPlaylistsBinding
 import com.example.playlistmaker.media.presentation.PlaylistsViewModel
 import com.example.playlistmaker.media.presentation.models.PlaylistsState
@@ -13,22 +15,23 @@ import org.koin.androidx.viewmodel.ext.android.viewModel
 
 class PlaylistsFragment : Fragment() {
 
-    companion object {
-        fun newInstance() = PlaylistsFragment()
-    }
-
     private val viewModel by viewModel<PlaylistsViewModel>()
 
-    private lateinit var binding: FragmentPlaylistsBinding
+    private var _binding: FragmentPlaylistsBinding? = null
+    private val binding get() = _binding!!
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
-                              savedInstanceState: Bundle?): View {
-        binding = FragmentPlaylistsBinding.inflate(inflater, container, false)
+                              savedInstanceState: Bundle?): View? {
+        _binding = FragmentPlaylistsBinding.inflate(inflater, container, false)
         return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        binding.newPlaylistButton.setOnClickListener {
+            findNavController().navigate(R.id.action_mediaFragment_to_playlistCreatorFragment)
+        }
 
         viewModel.observeState().observe(viewLifecycleOwner) {
             when(it) {
@@ -39,10 +42,18 @@ class PlaylistsFragment : Fragment() {
         }
     }
 
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+
     private fun showErrorMessage() {
         binding.apply {
             errorMessage.isVisible = true
         }
     }
 
+    companion object {
+        fun newInstance() = PlaylistsFragment()
+    }
 }
