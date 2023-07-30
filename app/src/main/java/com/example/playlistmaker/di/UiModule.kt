1e32aff30ae6abef5b130280bfe9a1364245d8a4
pogonat <aimagenta81@gmail.com>
2023-07-31 03:16:18 +0600

    package and name fixed in ThemeManager

--- app/src/main/java/com/example/playlistmaker/di/UiModule.kt
+++ app/src/main/java/com/example/playlistmaker/di/UiModule.kt
@@ -1,12 +1,13 @@
 package com.example.playlistmaker.di
 
 import android.media.MediaPlayer
-import com.example.playlistmaker.ThemeManager
+import com.example.playlistmaker.core.ThemeManager
 import com.example.playlistmaker.media.presentation.FavouritesViewModel
 import com.example.playlistmaker.media.presentation.PlaylistsViewModel
 import com.example.playlistmaker.player.presentation.PlayerViewModel
 import com.example.playlistmaker.player.presentation.TrackPlayer
 import com.example.playlistmaker.player.presentation.TrackPlayerImpl
+import com.example.playlistmaker.playlist.presentation.PlaylistCreatorViewModel
 import com.example.playlistmaker.search.presentation.SearchViewModel
 import com.example.playlistmaker.settings.presentation.SettingsViewModel
 import org.koin.androidx.viewmodel.dsl.viewModel
@@ -40,6 +41,10 @@ val uiModule = module {
 
     viewModel<FavouritesViewModel> {
         FavouritesViewModel(favInteractor = get())
+    }
+
+    viewModel<PlaylistCreatorViewModel> {
+        PlaylistCreatorViewModel(playlistInteractor = get())
     }
 
     single {
