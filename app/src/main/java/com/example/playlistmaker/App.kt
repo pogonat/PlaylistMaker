acca9afb4f7dfa0771a85669d77b5ff9c4c218ab
pogonat <aimagenta81@gmail.com>
2023-08-01 03:37:47 +0600

    fixed import

--- app/src/main/java/com/example/playlistmaker/App.kt
+++ app/src/main/java/com/example/playlistmaker/App.kt
@@ -1,6 +1,7 @@
 package com.example.playlistmaker
 
 import android.app.Application
+import com.example.playlistmaker.core.ThemeManager
 import com.example.playlistmaker.di.dataModule
 import com.example.playlistmaker.di.domainModule
 import com.example.playlistmaker.di.uiModule
@@ -12,8 +13,6 @@ import org.koin.core.logger.Level
 
 class App : Application() {
 
-    private val themeManager: ThemeManager by inject()
-
     override fun onCreate() {
         super.onCreate()
 
@@ -23,6 +22,7 @@ class App : Application() {
             modules(listOf(uiModule, domainModule, dataModule))
         }
 
+        val themeManager: ThemeManager by inject()
         themeManager.applyTheme()
 
     }
