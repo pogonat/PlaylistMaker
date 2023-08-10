65f7e1b66d285a707d3893dd0c181b1070fcfe9d
pogonat <aimagenta81@gmail.com>
2023-08-11 03:35:55 +0600

    add PlaylistContents state class

---
+++ app/src/main/java/com/example/playlistmaker/playlist/presentation/models/PlaylistContentsState.kt
@@ -0,0 +1,17 @@
+package com.example.playlistmaker.playlist.presentation.models
+
+import com.example.playlistmaker.domain.models.Playlist
+import com.example.playlistmaker.presentation.models.TrackUIModel
+
+sealed class PlaylistContentsState {
+    object Loading : PlaylistContentsState()
+
+    object Error : PlaylistContentsState()
+
+    data class Content(
+        val playlist: Playlist,
+        val trackList: List<TrackUIModel>?,
+        val playlistDuration: String?
+    ) : PlaylistContentsState()
+
+}
