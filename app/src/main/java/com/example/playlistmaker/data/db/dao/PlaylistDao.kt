a0dc18a6fa51fed9c803f9b45a1af32acb01dcf2
pogonat <aimagenta81@gmail.com>
2023-08-11 03:31:30 +0600

    fix playlist DAO

--- app/src/main/java/com/example/playlistmaker/data/db/dao/PlaylistDao.kt
+++ app/src/main/java/com/example/playlistmaker/data/db/dao/PlaylistDao.kt
@@ -16,8 +16,8 @@ interface PlaylistDao {
     fun updatePlaylist(trackList: String, quantity: Int, id: Int): Int
 
     @Query("SELECT * FROM playlist_table ORDER BY created_at DESC")
-    fun getPlaylists(): List<PlaylistEntity>
+    suspend fun getPlaylists(): List<PlaylistEntity>
 
     @Query("SELECT * FROM playlist_table WHERE playlistId=:id")
-    fun getPlaylistById(id: Int): PlaylistEntity
+    suspend fun getPlaylistById(id: Int): PlaylistEntity
 }
