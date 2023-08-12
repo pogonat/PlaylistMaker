cb71389258dd4499dba82148ca994fd2dec10722
pogonat <aimagenta81@gmail.com>
2023-08-13 05:04:59 +0600

    fix playlist cover

--- app/src/main/java/com/example/playlistmaker/playlist/ui/adapters/PlaylistViewHolder.kt
+++ app/src/main/java/com/example/playlistmaker/playlist/ui/adapters/PlaylistViewHolder.kt
@@ -1,6 +1,5 @@
 package com.example.playlistmaker.playlist.ui.adapters
 
-import android.util.Log
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
 import com.bumptech.glide.load.resource.bitmap.RoundedCorners
@@ -11,7 +10,7 @@ import com.example.playlistmaker.domain.models.Playlist
 class PlaylistViewHolder(
     private val binding: ListItemPlaylistBinding,
     private val clickListener: PlaylistAdapter.PlaylistClickListener
-): RecyclerView.ViewHolder(binding.root) {
+) : RecyclerView.ViewHolder(binding.root) {
 
     fun bind(playlist: Playlist) {
 
@@ -19,16 +18,19 @@ class PlaylistViewHolder(
 
         binding.tracksQuantity.text = playlist.tracksQuantityText
 
-        if (playlist.imagePath.isNotEmpty()) {
+
+        if (playlist.imagePath.isNotEmpty() == true) {
             Glide.with(binding.playlistCover)
                 .load(playlist.imagePath)
                 .centerCrop()
                 .transform(RoundedCorners(5))
                 .placeholder(R.drawable.placeholder_image)
                 .into(binding.playlistCover)
+        } else {
+            binding.playlistCover.setImageResource(R.drawable.placeholder_image)
         }
-        itemView.setOnClickListener{ clickListener.onPlaylistClick(playlist = playlist)
-        Log.e("CLICKED", "${playlist}")}
+
+        itemView.setOnClickListener { clickListener.onPlaylistClick(playlist = playlist) }
 
     }
 
